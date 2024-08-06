7af46bde61d780eac0b8cdf41679f0f6dfe4313d
cmdssd <168167043+cmdssd@users.noreply.github.com>
2024-08-06 12:00:11 +0800

    fix: svg在框架分批提交渲染时，无法正常工作 (#271)
    Co-authored-by: kongchuiyu <kongchuiyu@h-partners.com>

--- tester/harmony/svg/src/main/cpp/componentInstances/RNSVGSvgViewComponentInstance.h
+++ tester/harmony/svg/src/main/cpp/componentInstances/RNSVGSvgViewComponentInstance.h
@@ -20,11 +20,13 @@ public:
     // get SvgNode from childComponentInstance and set it to root_
     void onChildInserted(ComponentInstance::Shared const &childComponentInstance, std::size_t index) override {
         OnChildInsertCommon(std::dynamic_pointer_cast<SvgHost>(childComponentInstance));
+        getLocalRootArkUINode().markDirty();
     }
 
     // TODO get SvgNode and delete it from svg tree
     void onChildRemoved(ComponentInstance::Shared const &childComponentInstance) override {
         OnChildRemoveCommon(std::dynamic_pointer_cast<SvgHost>(childComponentInstance));
+        getLocalRootArkUINode().markDirty();
     }
 
     SvgArkUINode &getLocalRootArkUINode() override;
