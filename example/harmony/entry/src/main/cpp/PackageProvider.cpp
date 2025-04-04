92bc2f9af1ff50966228b6650461c53fe47eb4fc
Louis-C7 <chenlixi7@outlook.com>
2025-04-04 20:49:11 +0800

    chore: add @react-native-oh-tpl/react-native-reanimated package

--- example/harmony/entry/src/main/cpp/PackageProvider.cpp
+++ example/harmony/entry/src/main/cpp/PackageProvider.cpp
@@ -1,8 +1,9 @@
 #include "RNOH/PackageProvider.h"
+#include "ReanimatedPackage.h"
 #include "SVGPackage.h"
 
 using namespace rnoh;
 
 std::vector<std::shared_ptr<Package>> PackageProvider::getPackages(Package::Context ctx) {
-    return {std::make_shared<SVGPackage>(ctx)};
+    return {std::make_shared<SVGPackage>(ctx), std::make_shared<ReanimatedPackage>(ctx)};
 }
