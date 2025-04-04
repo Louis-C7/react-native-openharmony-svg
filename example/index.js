388b0831cee6b82f49e8cd4e096e6cf94705279a
Louis-C7 <chenlixi7@outlook.com>
2025-04-04 20:49:45 +0800

    chore: add swm apps

--- example/index.js
+++ example/index.js
@@ -7,6 +7,11 @@ import App from './App';
 import SVGDemo from './SVGDemo/SVGViewDemo';
 import {name as appName} from './app.json';
 import SvgDemoCases from './svgDemoCases/App'
+import TestExamples from './apps/test-examples/App'
+import Examples from './apps/examples/App'
+
 // AppRegistry.registerComponent(appName, () => App);
 AppRegistry.registerComponent(appName, () => SVGDemo);
 AppRegistry.registerComponent(appName, () => SvgDemoCases);
+AppRegistry.registerComponent(appName, () => TestExamples);
+AppRegistry.registerComponent(appName, () => Examples);
